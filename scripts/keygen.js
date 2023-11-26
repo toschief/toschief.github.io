d1592160414d05c6d4da4da5bf0e77ce99f67baf
ToSchief <118831917+ToSchief@users.noreply.github.com>
2023-11-26 15:39:54 +0100

    keygen fix the second

--- scripts/keygen.js
+++ scripts/keygen.js
@@ -1,17 +1,28 @@
 function generateKey() {
-    const keyLength = document.getElementById("keygeninput").innerText.length;
-    const key = generateRandomVigenereKey(keyLength);
-    document.getElementById('keygenoutput').innerText = key;
+  const keyLength = document.getElementById("keygeninput").value.length;
+  const key = generateRandomVigenereKey(keyLength);
+  document.getElementById("keygenoutput").innerText = key;
 }
 
 function generateRandomVigenereKey(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let key = '';
+  let characters = "";
 
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        key += characters.charAt(randomIndex);
-    }
+  if (document.getElementById("c1").checked) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (document.getElementById("c2").checked) {
+    characters += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (document.getElementById("c3").checked) {
+    characters += "0123456789";
+  }
 
-    return key;
+  let key = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    key += characters.charAt(randomIndex);
+  }
+
+  return key;
 }
